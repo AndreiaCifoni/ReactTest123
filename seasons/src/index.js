2b34e2ac5578dd98f63137e930736c91421c34be
Andreia Cifoni <andreiacifoni@gmail.com>
2022-04-05 22:12:09 -0400

    error

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,26 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   //constructor to inicialize the state
   constructor(props) {
     super(props);
     //THIS IS THE ONLY TIME we do direct assignment to "this.state"
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         //we call setState to update the state
         this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   //React says we have to define render!!
   render() {
-    return <div>Latitude: {this.state.lat} </div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
