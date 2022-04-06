d67db14d429bcd41c4843d473a9dc49bf3e5c506
Andreia Cifoni <andreiacifoni@gmail.com>
2022-04-05 21:39:56 -0400

    state system

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,13 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends React.Component {
+  //constructor to inicialize the state
+  constructor(props) {
+    super(props);
+    //THIS IS THE ONLY TIME we do direct assignment to "this.state"
+    this.state = { lat: null };
 
-  return <div>Hi there!</div>;
-};
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //we call setState to update the state
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  //React says we have to define render!!
+  render() {
+    return <div>Latitude: {this.state.lat} </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
