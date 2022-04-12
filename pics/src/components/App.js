5ec44104cafcb4cfe38e45e95bc991888cccd73c
Andreia Cifoni <andreiacifoni@gmail.com>
2022-04-11 21:43:13 -0400

    async request, binding callback

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,20 +1,17 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   state = { images: [] };
 
-  async onSearchSubmit(term) {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get("/search/photos", {
       params: { query: term },
-      headers: {
-        Authorization: "Client-ID BNBcIUXLw5cOI63UNWLqWHJSJzmXRmLc7nYpiVVinlM",
-      },
     });
 
     this.setState({ images: response.data.results });
-  }
+  };
 
   render() {
     return (
