febdcd8a4594ca3eb1c19ee8b8332d2d663c356f
Andreia Cifoni <andreiacifoni@gmail.com>
2022-04-14 15:29:48 -0400

    starting youtube API onSubmitForm

---
+++ videos/src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyDnVHjqt0Wce96mhX6sxIKOQmzIUFiDZZM";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: "5",
+    key: KEY,
+  },
+});
--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  // state = { videos: [] };
+
+  // onSearchSubmit = async () => {
+  //   const response = await youtube.get("url", {
+  //     params: { query, term },
+  //   });
+  // };
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    );
   }
 }
 
