47868f54d0000a803537162685cc8fc47e79f112
Andreia Cifoni <andreiacifoni@gmail.com>
2022-05-06 22:06:57 -0400

    new app to learn Context system

---
+++ translate/src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import UserCreate from "./UserCreate";
+
+class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select the language:
+          <i
+            onClick={() => this.onLanguageChange("english")}
+            className="flag us"
+          />
+          <i
+            onClick={() => this.onLanguageChange("dutch")}
+            className="flag nl"
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
+
+export default App;
